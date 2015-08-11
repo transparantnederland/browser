e263f51655560d84187669cffc98e0a52bbe0585
Bert Spaan <bert.spaan@gmail.com>
2015-08-11 13:43:54 +0200

    first working draft finished

--- build/relationizer.js
+++ build/relationizer.js
@@ -3,23 +3,33 @@ var Relationizer = React.createClass({displayName: "Relationizer",
     return (
       React.createElement("div", {className: "container"}, 
         React.createElement("div", {id: "object1", className: "col"}, 
-          React.createElement(ObjectSearch, {apiUrl: this.props.apiUrl})
+          React.createElement(ObjectSearch, {apiUrl: this.props.apiUrl, selectPit: this.selectPitFrom})
         ), 
         React.createElement("div", {id: "relation", className: "col"}, 
-          React.createElement(CreateRelation, {apiUrl: this.props.apiUrl})
+          React.createElement(CreateRelation, {apiUrl: this.props.apiUrl, ref: "createRelation"})
         ), 
         React.createElement("div", {id: "object2", className: "col"}, 
-          React.createElement(ObjectSearch, {apiUrl: this.props.apiUrl})
+          React.createElement(ObjectSearch, {apiUrl: this.props.apiUrl, selectPit: this.selectPitTo})
         )
       )
     );
+  },
+
+  selectPitFrom: function(pit) {
+    this.refs.createRelation.setPitFrom(pit);
+  },
+
+  selectPitTo: function(pit) {
+    this.refs.createRelation.setPitTo(pit);
   }
+
 });
 
 var ObjectSearch = React.createClass({displayName: "ObjectSearch",
   getInitialState: function() {
     return {
-      timeout: null
+      timeout: null,
+      query: ''
     };
   },
 
@@ -32,12 +42,12 @@ var ObjectSearch = React.createClass({displayName: "ObjectSearch",
     return (
       React.createElement("div", null, 
         React.createElement("input", {ref: "search"}), 
-        React.createElement("ul", null, 
+        React.createElement("ul", {className: "concepts"}, 
           features.map(function (feature, index) {
-            return React.createElement("li", null, 
-              React.createElement(Feature, {feature: feature, key: index})
+            return React.createElement("li", {className: "concept", key: this.state.query + index}, 
+              React.createElement(Feature, {feature: feature, selectPit: this.props.selectPit})
             );
-          })
+          }.bind(this))
         )
       )
     );
@@ -69,7 +79,8 @@ var ObjectSearch = React.createClass({displayName: "ObjectSearch",
     var q = this.refs.search.getDOMNode().value;
     d3.json(this.props.apiUrl + 'search?q=' + q, function(geojson) {
       this.setState({
-        geojson: geojson
+        geojson: geojson,
+        query: q
       });
     }.bind(this));
   }
@@ -77,44 +88,89 @@ var ObjectSearch = React.createClass({displayName: "ObjectSearch",
 });
 
 var Feature = React.createClass({displayName: "Feature",
+
+  getInitialState: function() {
+    var feature = this.props.feature;
+    var sortedNames = this.sortNames(feature.properties.pits);
+    var selectedName = sortedNames[0].name;
+    return {
+      name: selectedName,
+      type: feature.properties.pits[0].type.replace('hg:', '')
+    };
+  },
+
   render: function() {
     return (
-      React.createElement("ul", null, 
-      this.props.feature.properties.pits.map(function (pit) {
-        return React.createElement("li", null, 
-          React.createElement(Pit, {pit: pit, key: pit.id || pit.uri})
-        );
-      })
+      React.createElement("div", null, 
+        React.createElement("h2", null, 
+          React.createElement("span", null, this.state.name), 
+          React.createElement("span", {className: "type"}, this.state.type)
+        ), 
+        React.createElement("ul", {className: "pits"}, 
+        this.props.feature.properties.pits.map(function (pit) {
+          return React.createElement("li", {className: "pit", key: pit.id || pit.uri}, 
+            React.createElement(Pit, {pit: pit, selectPit: this.props.selectPit})
+          );
+        }.bind(this))
+        )
       )
     );
+  },
+
+  sortNames: function(pits) {
+    var names = pits.map(function(pit) { return pit.name; });
+    var counts = {};
+
+    for (var k = 0, j = names.length; k < j; k++) {
+      counts[names[k]] = (counts[names[k]] || 0) + 1;
+    }
+
+    return Object.keys(counts).map(function(name) {
+      return {
+        name: name,
+        count: counts[name]
+      };
+    }).sort(function(a, b) {
+      return b.count - a.count;
+    });
   }
+
 });
 
 var Pit = React.createClass({displayName: "Pit",
+
   render: function() {
     return (
-      React.createElement("div", null, this.props.pit.name || this.props.pit.id)
+      React.createElement("div", {onClick: this.select}, this.props.pit.name || this.props.pit.id)
     );
+  },
+
+  select: function() {
+    this.props.selectPit(this.props.pit);
   }
 });
 
 var CreateRelation = React.createClass({displayName: "CreateRelation",
   getInitialState: function() {
     return {
+      from: null,
+      to: null,
       relations: []
     };
   },
 
   render: function() {
     return (
-      React.createElement("ul", null, 
-      this.state.relations.map(function (relation) {
-        return React.createElement("li", null, 
-          React.createElement("div", {key: relation}, 
-        relation
-          )
-        );
-      })
+      React.createElement("div", null, 
+        React.createElement("select", {ref: "select", value: "hg:liesIn"}, 
+        this.state.relations.map(function (relation) {
+          return React.createElement("option", {key: relation, value: relation}, 
+            relation
+          );
+        }.bind(this))
+        ), 
+        React.createElement("button", {className: "btn btn-1 btn-1e", onClick: this.createRelation}, "Create!"), 
+        React.createElement("textarea", {ref: "relations"})
       )
     );
   },
@@ -127,10 +183,41 @@ var CreateRelation = React.createClass({displayName: "CreateRelation",
         });
       }
     }.bind(this));
+  },
+
+  createRelation: function() {
+    var from = this.state.from ? (this.state.from.id || this.state.from.uri) : null;
+    var to = this.state.to ? (this.state.to.id || this.state.to.uri) : null;
+    var type = this.refs.select.getDOMNode().value;
+
+    var relation = {
+      from: from,
+      to: to,
+      type: type
+    };
+
+    this.refs.relations.getDOMNode().value += '\n' + JSON.stringify(relation);
+
+
+  },
+
+  setPitFrom: function(pit) {
+    this.setState({
+      from: pit
+    });
+  },
+
+  setPitTo: function(pit) {
+    this.setState({
+      to: pit
+    });
   }
 });
 
+var apiUrl = 'http://localhost:3001/';
+// var apiUrl = 'http://vps662.directvps.nl:3000/';
+
 React.render(
-  React.createElement(Relationizer, {apiUrl: "http://vps662.directvps.nl:3000/"}),
+  React.createElement(Relationizer, {apiUrl: apiUrl}),
   document.getElementById('relationizer')
 );
--- src/relationizer.js
+++ src/relationizer.js
@@ -3,23 +3,33 @@ var Relationizer = React.createClass({
     return (
       <div className='container'>
         <div id='object1' className='col'>
-          <ObjectSearch apiUrl={this.props.apiUrl} />
+          <ObjectSearch apiUrl={this.props.apiUrl} selectPit={this.selectPitFrom} />
         </div>
         <div id='relation' className='col'>
-          <CreateRelation apiUrl={this.props.apiUrl} />
+          <CreateRelation apiUrl={this.props.apiUrl} ref='createRelation' />
         </div>
         <div id='object2' className='col'>
-          <ObjectSearch apiUrl={this.props.apiUrl} />
+          <ObjectSearch apiUrl={this.props.apiUrl} selectPit={this.selectPitTo} />
         </div>
       </div>
     );
+  },
+
+  selectPitFrom: function(pit) {
+    this.refs.createRelation.setPitFrom(pit);
+  },
+
+  selectPitTo: function(pit) {
+    this.refs.createRelation.setPitTo(pit);
   }
+
 });
 
 var ObjectSearch = React.createClass({
   getInitialState: function() {
     return {
-      timeout: null
+      timeout: null,
+      query: ''
     };
   },
 
@@ -32,12 +42,12 @@ var ObjectSearch = React.createClass({
     return (
       <div>
         <input ref='search' />
-        <ul>
+        <ul className='concepts'>
           {features.map(function (feature, index) {
-            return <li>
-              <Feature feature={feature} key={index} />
+            return <li className='concept' key={this.state.query + index}>
+              <Feature feature={feature} selectPit={this.props.selectPit} />
             </li>;
-          })}
+          }.bind(this))}
         </ul>
       </div>
     );
@@ -69,7 +79,8 @@ var ObjectSearch = React.createClass({
     var q = this.refs.search.getDOMNode().value;
     d3.json(this.props.apiUrl + 'search?q=' + q, function(geojson) {
       this.setState({
-        geojson: geojson
+        geojson: geojson,
+        query: q
       });
     }.bind(this));
   }
@@ -77,45 +88,90 @@ var ObjectSearch = React.createClass({
 });
 
 var Feature = React.createClass({
+
+  getInitialState: function() {
+    var feature = this.props.feature;
+    var sortedNames = this.sortNames(feature.properties.pits);
+    var selectedName = sortedNames[0].name;
+    return {
+      name: selectedName,
+      type: feature.properties.pits[0].type.replace('hg:', '')
+    };
+  },
+
   render: function() {
     return (
-      <ul>
-      {this.props.feature.properties.pits.map(function (pit) {
-        return <li>
-          <Pit pit={pit} key={pit.id || pit.uri} />
-        </li>;
-      })}
-      </ul>
+      <div>
+        <h2>
+          <span>{this.state.name}</span>
+          <span className='type'>{this.state.type}</span>
+        </h2>
+        <ul className='pits'>
+        {this.props.feature.properties.pits.map(function (pit) {
+          return <li className='pit' key={pit.id || pit.uri}>
+            <Pit pit={pit} selectPit={this.props.selectPit} />
+          </li>;
+        }.bind(this))}
+        </ul>
+      </div>
     );
+  },
+
+  sortNames: function(pits) {
+    var names = pits.map(function(pit) { return pit.name; });
+    var counts = {};
+
+    for (var k = 0, j = names.length; k < j; k++) {
+      counts[names[k]] = (counts[names[k]] || 0) + 1;
+    }
+
+    return Object.keys(counts).map(function(name) {
+      return {
+        name: name,
+        count: counts[name]
+      };
+    }).sort(function(a, b) {
+      return b.count - a.count;
+    });
   }
+
 });
 
 var Pit = React.createClass({
+
   render: function() {
     return (
-      <div>{this.props.pit.name || this.props.pit.id}</div>
+      <div onClick={this.select}>{this.props.pit.name || this.props.pit.id}</div>
     );
+  },
+
+  select: function() {
+    this.props.selectPit(this.props.pit);
   }
 });
 
 var CreateRelation = React.createClass({
   getInitialState: function() {
     return {
+      from: null,
+      to: null,
       relations: []
     };
   },
 
   render: function() {
     return (
-      <ul>
-      {this.state.relations.map(function (relation) {
-        return <li>
-          <div key={relation}>
-        {relation}
-          </div>
-        </li>;
-      })}
-      </ul>
+      <div>
+        <select ref='select' value='hg:liesIn'>
+        {this.state.relations.map(function (relation) {
+          return <option key={relation} value={relation}>
+            {relation}
+          </option>;
+        }.bind(this))}
+        </select>
+        <button className="btn btn-1 btn-1e" onClick={this.createRelation}>Create!</button>
+        <textarea ref='relations' />
+      </div>
     );
   },
 
@@ -127,10 +183,41 @@ var CreateRelation = React.createClass({
         });
       }
     }.bind(this));
+  },
+
+  createRelation: function() {
+    var from = this.state.from ? (this.state.from.id || this.state.from.uri) : null;
+    var to = this.state.to ? (this.state.to.id || this.state.to.uri) : null;
+    var type = this.refs.select.getDOMNode().value;
+
+    var relation = {
+      from: from,
+      to: to,
+      type: type
+    };
+
+    this.refs.relations.getDOMNode().value += '\n' + JSON.stringify(relation);
+
+
+  },
+
+  setPitFrom: function(pit) {
+    this.setState({
+      from: pit
+    });
+  },
+
+  setPitTo: function(pit) {
+    this.setState({
+      to: pit
+    });
   }
 });
 
+var apiUrl = 'http://localhost:3001/';
+// var apiUrl = 'http://vps662.directvps.nl:3000/';
+
 React.render(
-  <Relationizer apiUrl='http://vps662.directvps.nl:3000/'/>,
+  <Relationizer apiUrl={apiUrl} />,
   document.getElementById('relationizer')
 );
