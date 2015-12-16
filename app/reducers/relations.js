0e96e94590820a26c0bc027cac53eddbdcea7db5
Tim Bormans <hello@timbormans.com>
2015-12-16 13:30:09 +0100

    Add redux persist (localStorage) layer

--- app/reducers/relations.js
+++ app/reducers/relations.js
@@ -1,7 +1,10 @@
+import { REHYDRATE } from 'redux-persist/constants';
 import * as actions from './../actions/relations';
 
 function relations(state = [], action) {
   switch (action.type) {
+    case REHYDRATE:
+      return (action.key === 'relations' && action.payload) || state;
     case actions.ADD_RELATION:
       return [...state, action.payload.relation];
     default:
--- app/store/index.js
+++ app/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
+import { persistStore } from 'redux-persist';
+
 import api from './api';
 import relations from './../reducers/relations';
 
@@ -13,5 +15,7 @@ const reducer = combineReducers(Object.assign({
   relations,
 }, api.reducers));
 const store = createStoreWithMiddleware(reducer);
+
+persistStore(store);
 
 export default store;
