9eca7adc544beda18c26f82083f728b9c750321d
Tim Bormans <hello@timbormans.com>
2015-12-16 13:40:28 +0100

    Add propTypes to Pit component

--- app/components/Pit.js
+++ app/components/Pit.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export default React.createClass({
+  propTypes: {
+    pit: PropTypes.object.isRequired,
+  },
+
   render() {
+    const { name, id, uri, dataset } = this.props.pit;
     return (
-      <div className="pad-all" onClick={this.select}>
-        <h4>{this.props.pit.name || this.props.pit.id}</h4>
+      <div className="pad-all" onClick={this.handleClick}>
+        <h4>{name || id}</h4>
         <div className="table-container">
           <table>
             <tbody>
               <tr>
                 <td className="label">Dataset</td>
-                <td><code>{this.props.pit.dataset}</code></td>
+                <td><code>{dataset}</code></td>
               </tr>
               <tr>
                 <td className="label">ID</td>
-                <td>{this.props.pit.id || this.props.pit.uri}</td>
+                <td>{id || uri}</td>
               </tr>
             </tbody>
           </table>
@@ -22,7 +27,8 @@ export default React.createClass({
       </div>
     );
   },
-  select() {
+
+  handleClick() {
     this.props.selectPit(this.props.pit);
   },
 });
