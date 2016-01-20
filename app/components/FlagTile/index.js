3b50527d0ba2a4e183c52397328065b96a470f36
Tim Bormans <hello@timbormans.com>
2016-01-20 15:19:16 +0100

    Improve flag styling

--- app/components/FlagTile/index.js
+++ app/components/FlagTile/index.js
@@ -3,14 +3,19 @@ import React, { PropTypes } from 'react';
 
 import ConceptTile from '../ConceptTile';
 
+const flagTypes = {
+  'missing-relation': 'add',
+  'duplicate': 'add',
+};
+
 const FlagTile = ({ flag }) => {
   return (
-    <div className="FlagTile">
+    <div className={['FlagTile', 'FlagTile--' + flagTypes[flag.type]].join(' ')}>
       <div className="FlagTile-from">
         <ConceptTile concept={flag.origin} />
       </div>
       <div className="FlagTile-type">
-        {flag.type}
+        {flag.value}
       </div>
       <div className="FlagTile-to">
         <ConceptTile concept={flag.target} />
