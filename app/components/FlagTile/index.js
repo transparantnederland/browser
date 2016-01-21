dad29a42cf77ac96f66d07c0a1e2400524cf650a
Tim Bormans <hello@timbormans.com>
2016-01-21 11:55:59 +0100

    Add more info to flags list

--- app/components/FlagTile/index.js
+++ app/components/FlagTile/index.js
@@ -6,19 +6,30 @@ import ConceptTile from '../ConceptTile';
 const flagTypes = {
   'missing-relation': 'add',
   'duplicate': 'add',
+  'wrong-type': 'edit',
 };
 
-const FlagTile = ({ flag }) => {
+const FlagTile = ({ flag: { type, origin, value, target } }) => {
+  const isWrongType = type === 'wrong-type';
+
   return (
-    <div className={['FlagTile', 'FlagTile--' + flagTypes[flag.type]].join(' ')}>
+    <div className={['FlagTile', 'FlagTile--' + flagTypes[type]].join(' ')}>
       <div className="FlagTile-from">
-        <ConceptTile concept={flag.origin} />
+        <ConceptTile concept={origin} />
       </div>
       <div className="FlagTile-type">
-        {flag.value}
+        <dl>
+          <dt>type</dt>
+          <dd>{type}</dd>
+          <dt>value</dt>
+          <dd>{value}</dd>
+        </dl>
       </div>
       <div className="FlagTile-to">
-        <ConceptTile concept={flag.target} />
+        <ConceptTile concept={Object.assign(target, {
+          type: isWrongType ? value : target.type,
+        })}
+        />
       </div>
     </div>
   );
