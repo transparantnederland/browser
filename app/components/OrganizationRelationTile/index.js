c60874c468a7ba32645ba89c647b15f53309ecb0
Tim Bormans <hello@timbormans.com>
2016-01-25 17:06:28 +0100

    Add since/until information to organizations

--- app/components/OrganizationRelationTile/index.js
+++ app/components/OrganizationRelationTile/index.js
@@ -1,11 +1,14 @@
+import './index.css';
 import React, { PropTypes } from 'react';
 
 import HashLink from '../HashLink';
 import Name from '../Name';
+import RelationPeriod from '../RelationPeriod';
 
 const RELATIONS = {
   'tnl:parent': 'Parent Company',
   'tnl:member': 'Member',
+  'tnl:related': 'Related',
   'tnl:boardmember': 'Board member',
   'tnl:commissioner': 'Commissioner',
   'tnl:advisor': 'Advisor',
@@ -13,14 +16,14 @@ const RELATIONS = {
   'tnl:lobbyist': 'Lobbyist',
 };
 
-const RelationTile = ({ relation: { concept, relation: { type } } }) => {
+const RelationTile = ({ relation: { concept, relation: { type, since, until } } }) => {
   return (
-    <div>
+    <div className="OrganizationRelationTile">
       <HashLink hash={concept.id}>
         <Name name={concept.name}/>
-      </HashLink>
-      <div>
-        {RELATIONS[type]}
+      </HashLink> ({RELATIONS[type]})
+      <div className="OrganizationRelationTile-body">
+        {since ? <RelationPeriod since={since} until={until} /> : null}
       </div>
     </div>
   );
