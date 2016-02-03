9daabe5f1ee18034d7cb9237707e984e40653472
Tim Bormans <hello@timbormans.com>
2016-02-03 17:58:46 +0100

    Disallow concept to be dropped on same concept

--- app/components/ConceptDropTarget/index.js
+++ app/components/ConceptDropTarget/index.js
@@ -2,14 +2,17 @@ import './index.css';
 import React from 'react';
 import { DropTarget } from 'react-dnd';
 
-const ConceptDropTarget = ({ connectDropTarget, isOver, children }) =>
+const ConceptDropTarget = ({ connectDropTarget, isOver, canDrop, children }) =>
   connectDropTarget(
     <div className="ConceptDropTarget">
-      {isOver ? <div className="ConceptDropTarget-overlay">
-        <div className="ConceptDropTarget-message">
-          Drop here to create a relation.
-        </div>
-      </div> : null}
+      {isOver ? <div className={['ConceptDropTarget-overlay', !canDrop ? 'ConceptDropTarget-overlay--disabled' : ''].join(' ')}>
+        {canDrop ?
+          <div className="ConceptDropTarget-message">
+            Drop here to create a relation.
+          </div> : null
+        }
+      </div> : null
+      }
       <div className="ConceptDropTarget-content">
         {children}
       </div>
@@ -17,6 +20,13 @@ const ConceptDropTarget = ({ connectDropTarget, isOver, children }) =>
   );
 
 const target = {
+  canDrop(props, monitor) {
+    const item = monitor.getItem();
+    const sourceConcept = item.concept;
+    const targetConcept = props.concept;
+
+    return sourceConcept.id !== targetConcept.id;
+  },
   drop(props) {
     return {
       concept: props.concept,
